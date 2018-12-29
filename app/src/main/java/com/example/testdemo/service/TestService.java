fae21e512ee415c02f3b56a8b2ea410b57401c3b
Nature_Day <1004934782@qq.com>
2018-12-29 17:30:29 +0800

    update

--- app/src/main/java/com/example/testdemo/service/TestService.java
+++ app/src/main/java/com/example/testdemo/service/TestService.java
@@ -2,7 +2,9 @@ package com.example.testdemo.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
+import android.os.Parcel;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -26,12 +28,17 @@ public class TestService extends Service {
         super.onDestroy();
     }
 
-    @Override
-    public boolean onUnbind(Intent intent) {
-        return super.onUnbind(intent);
-    }
-
     private final ITestInterface.Stub mBinder = new ITestInterface.Stub() {
+
+        @Override
+        public boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+            if (checkCallingPermission("com.example.testdemo.aidl.permission.OPERATION")
+                    != PackageManager.PERMISSION_GRANTED) {
+                Log.i("fff", "------TestService---mBinder--------onTransact---未获得权限---");
+                return false;
+            }
+            return super.onTransact(code, data, reply, flags);
+        }
 
         @Override
         public void connect(String msg) throws RemoteException {
